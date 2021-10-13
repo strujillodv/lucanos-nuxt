55310663bcfba50fbcea27383d8857b0c291c71a
Sergio Antonio Trujillo del Valle <strujillodv@gmail.com>
2021-10-13 11:02:02 -0500

    create component for newsleter

---
+++ components/Newsleter.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  section
+    v-row(
+        style="z-index: 1;"
+        no-gutters
+        aling="center"
+      )
+        v-col(
+          class="col-10 col-sm-4  d-flex justify-center"
+        )
+          lottie-vue-player(
+            style="max-width: 290px;"
+            src="/lottie/newsletter.json"
+          )
+        v-col(
+          class="col-12 col-sm-6 offset-sm-1 d-flex justify-center align-self-center"
+          aling="center"
+        )
+</template>
