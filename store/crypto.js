2b59ae925f406a87e512c535c6975fc26bbc2121
Sergio Antonio Trujillo del Valle <strujillodv@gmail.com>
2021-10-15 22:16:54 -0500

    create store for crypto data

---
+++ store/crypto.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+export const state = () => ({
+  coins: []
+})
+
+export const mutations = {
+  addData(state, dataCoins) {
+    state.coins = dataCoins
+  }
+}
+
+export const actions = {
+  getCoins ({ commit }) {
+    axios
+      .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+      .then(response => {
+        commit('addPost', response.data)
+    })
+  }
+}
