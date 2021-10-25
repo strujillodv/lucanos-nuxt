476ae9861739e53eb7ea56da4505824e3a42638d
Sergio Antonio Trujillo del Valle <strujillodv@gmail.com>
2021-10-24 22:09:51 -0500

    create component

---
+++ components/SocialButtons.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+div
+  v-btn(
+    v-for="(item, index) in social"
+    :key="index"
+    :href="item.link"
+    target="_blank"
+    icon
+    @click="$emit('click', drawer)"
+  )
+    v-icon mdi-{{item.name}}
+
+</template>
+<script>
+export default {
+  data: () => ({
+    social: [
+      {
+        name: "facebook",
+        link: "https://www.facebook.com/Lucanos-107479854984571"
+      },
+      {
+        name: "instagram",
+        link: "https://www.instagram.com/lucanos_/"
+      },
+      {
+        name: "twitter",
+        link: "https://twitter.com/Lucanos_"
+      },
+      {
+        name: "youtube",
+        link: "https://www.youtube.com/channel/UC0XcbFpOy_ZaYvH2ulzz78A"
+      }
+    ]
+  })
+}
+</script>
