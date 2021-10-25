94136291d71138951f6020e85c5d0841583269b9
Sergio Antonio Trujillo del Valle <strujillodv@gmail.com>
2021-10-24 22:09:22 -0500

    creation crypto wiget

---
+++ components/CarouselCrypto.vue
@@ -0,0 +1,82 @@
+<template lang="pug">
+  div.slider
+    div.slide-track
+      div.slide(
+        v-for="(n, i) in coins"
+        :key="i"
+      )
+        v-btn(
+          text
+          small
+        )
+          v-avatar(tile size="20")
+            v-img(:src="n.image")
+          span &nbsp; {{ n.symbol }} - ${{ n.current_price }} -
+          span(
+            :class="[ n.price_change_percentage_24h > 0 ? 'red--text' : 'green--text']"
+          ) &nbsp;{{n.price_change_percentage_24h}}%
+</template>
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed: mapState({
+    'coins': state => state.crypto.coins
+  }),
+}
+</script>
+<style>
+
+@-webkit-keyframes scroll {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(calc(-250px * 7));
+  }
+}
+
+@keyframes scroll {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(calc(-250px * 7));
+  }
+}
+.slider {
+  background: white;
+  box-shadow: 0 10px 20px -5px rgba(0, 0, 0, 0.125);
+  height: 30px;
+  margin: auto;
+  overflow: hidden;
+  position: relative;
+  width: 100%;
+}
+.slider::before, .slider::after {
+  background: linear-gradient(to right, rgba(255, 255, 255, 0.3) 0%, rgba(255, 255, 255, 0) 100%);
+  content: "";
+  height: 100px;
+  position: absolute;
+  width: 200px;
+  z-index: 2;
+}
+.slider::after {
+  right: 0;
+  top: 0;
+  transform: rotateZ(180deg);
+}
+.slider::before {
+  left: 0;
+  top: 0;
+}
+.slider .slide-track {
+  -webkit-animation: scroll 40s linear infinite;
+          animation: scroll 40s linear infinite;
+  display: flex;
+  width: calc(250px * 14);
+}
+.slider .slide {
+  height: 100px;
+  width: 250px;
+}
+</style>
