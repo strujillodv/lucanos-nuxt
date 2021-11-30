9dd3b6d9ad3254c44bdd17826fc62a6004507a1e
Sergio Antonio Trujillo del Valle <strujillodv@gmail.com>
2021-11-30 10:06:28 -0500

    formulario de newsletter

---
+++ components/FormMailchimp.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+  v-card(
+    elevation="0"
+    max-width="400"
+    width="100%"
+  )
+    v-card-text
+      v-form(
+        action="https://lucanos.us5.list-manage.com/subscribe/post?u=6aad02dd5cccc2eba44ee0c77&amp;id=19f558926d"
+        method="post" id="mc-embedded-subscribe-form"
+        target="_blank"
+        novalidate
+        color="primary"
+      )
+        v-text-field(
+          name="FNAME"
+          label="Nombre"
+          id="mce-LNAME"
+          required
+        )
+
+        v-text-field(
+          name="LNAME"
+          label="Apellido"
+          id="mce-FNAME"
+          required
+        )
+
+        v-text-field(
+          name="EMAIL"
+          label="correo:"
+          id="mce-EMAIL"
+          required
+        )
+
+        v-btn(
+          type="submit"
+          value="Subscribe"
+          name="subscribe"
+          id="mc-embedded-subscribe"
+          color="primary"
+        ) Suscribete
+
+</template>
