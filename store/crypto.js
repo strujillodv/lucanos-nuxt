7729cc1d59c2f3ea1dc7696f7b4dd418067eb97b
Sergio Antonio Trujillo del Valle <strujillodv@gmail.com>
2021-11-30 10:13:20 -0500

    store para el manejo de la api de coingecko

--- store/crypto.js
+++ store/crypto.js
@@ -9,11 +9,26 @@ export const mutations = {
     state.coins = dataCoins
   }
 }
+export const getters = {
+
+  search: (state) => (search) => {
+    if (search !== null || search !== undefined || search !== ""){
+      return state.coins.filter(coins =>
+        coins.name.toLowerCase().includes(search.toLowerCase()) ||
+        coins.symbol.toLowerCase().includes(search.toLowerCase())
+      )
+    } else {
+      return state.coins
+    }
+
+  }
+}
 
 export const actions = {
   getCoins ({ commit }) {
+    const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&price_change_percentage=1h,24h&order=market_cap_desc&per_page=50&page=1&sparkline=false'
     axios
-      .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false')
+      .get(url)
       .then(response => {
         commit('addData', response.data)
     })
