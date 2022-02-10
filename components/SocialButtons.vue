45f856aea0caa4d8c3fd9553504eb03c77102a6d
Sergio Antonio Trujillo del Valle <strujillodv@gmail.com>
2022-02-10 09:30:32 -0500

    se actualizan las redes sociales y se le da color a los iconos

--- components/SocialButtons.vue
+++ components/SocialButtons.vue
@@ -6,7 +6,10 @@ div
     :href="item.link"
     target="_blank"
     icon
+    :color="item.color"
     @click="$emit('click', drawer)"
+    large
+    class="mx-2"
   )
     v-icon mdi-{{item.name}}
 
@@ -17,19 +20,23 @@ export default {
     social: [
       {
         name: "facebook",
-        link: "https://www.facebook.com/Lucanos-107479854984571"
+        link: "https://www.facebook.com/Lucanos-107479854984571",
+        color: "primary"
       },
       {
         name: "instagram",
-        link: "https://www.instagram.com/lucanos_/"
+        link: "https://www.instagram.com/lucanos_/",
+        color: "pink"
       },
       {
         name: "twitter",
-        link: "https://twitter.com/Lucanos_"
+        link: "https://twitter.com/Lucanos_",
+        color: "primary"
       },
       {
         name: "youtube",
-        link: "https://www.youtube.com/channel/UC0XcbFpOy_ZaYvH2ulzz78A"
+        link: "https://www.youtube.com/channel/UC0XcbFpOy_ZaYvH2ulzz78A",
+        color: "red"
       }
     ]
   })
